ba33cc04d971cb9586ce96b74c8ceac1a7f9ca26
John Crisostomo <johncrisostomo@protonmail.ch>
2016-10-24 20:59:12 +0800

    adds another test for the fourth param

--- test/index.js
+++ test/index.js
@@ -16,37 +16,14 @@ describe('getDateRange', function() {
   });
 
   it('should return the correct array based on the format', function() {
-    const correctOutput = [ 'Sep 30',
-      'Sep 29',
-      'Sep 28',
-      'Sep 27',
-      'Sep 26',
-      'Sep 25',
-      'Sep 24',
-      'Sep 23',
-      'Sep 22',
-      'Sep 21',
-      'Sep 20',
-      'Sep 19',
-      'Sep 18',
-      'Sep 17',
-      'Sep 16',
-      'Sep 15',
-      'Sep 14',
-      'Sep 13',
-      'Sep 12',
-      'Sep 11',
-      'Sep 10',
-      'Sep 09',
-      'Sep 08',
-      'Sep 07',
-      'Sep 06',
-      'Sep 05',
-      'Sep 04',
-      'Sep 03',
-      'Sep 02',
-      'Sep 01' ];
+    const correctOutput = [ 'Sep 05', 'Sep 04', 'Sep 03', 'Sep 02', 'Sep 01' ];
 
-    getDateRange('09/01/2016', '09/30/2016', 'MMM DD').should.be.eql(correctOutput);
+    getDateRange('09/01/2016', '09/05/2016', 'MMM DD').should.be.eql(correctOutput);
+  });
+
+  it('should return an array sorted in ascending order if last param is true", function() {
+    const correctOutput = [ 'Sep 01', 'Sep 02', 'Sep 03', 'Sep 04', 'Sep 05' ];
+
+    getDateRange('09/01/2016', '09/05/2016', 'MMM DD', true).should.be.eql(correctOutput);
   });
 });
