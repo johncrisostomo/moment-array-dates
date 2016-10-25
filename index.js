eb5635d29ad5c4f3406669113afc0d784cf187cc
John Crisostomo <jd@snapzio.com>
2016-10-25 11:34:59 +0800

    adds two new functions for last n days and next n days

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var moment = require('moment');
 
-module.exports = function(startDate, endDate, dateFormat, ascending) {
+function range(startDate, endDate, dateFormat, ascending) {
   var dates = [];
   ascending = ascending || false;
 
@@ -18,10 +18,66 @@ module.exports = function(startDate, endDate, dateFormat, ascending) {
   for(var i = 0; i < difference; i++) {
     dates.push(end.subtract(1, 'd').format(dateFormat));
   }
-  
+
   if(ascending) {
     return dates.reverse();
   }
 
   return dates;
+}
+
+function lastNDays(days, dateFormat, ascending) {
+  if (days <= 0) {
+    throw Error("Invalid number of days specified. Please ensure a non-zero, positive integer for the days");
+  }
+
+  dates = [];
+
+  ascending = ascending || false;
+
+  var start = moment(new Date().setHours(0, 0, 0, 0)).subtract(days, 'd');
+  var end = moment(new Date().setHours(0, 0, 0, 0));
+
+  var difference = end.diff(start, 'days');
+
+  for(var i = 0; i < difference; i++) {
+    dates.push(end.subtract(1, 'd').format(dateFormat));
+  }
+
+  if (ascending) {
+    return dates.reverse();
+  }
+
+  return dates;
+}
+
+function nextNDays(days, dateFormat, ascending) {
+  if (days <= 0) {
+    throw Error("Invalid number of days specified. Please ensure a non-zero, positive integer for the days");
+  }
+
+  dates = [];
+
+  ascending = ascending || false;
+
+  var start = moment(new Date().setHours(0, 0, 0, 0)).add(1, 'd');
+  var end = moment(new Date().setHours(0, 0, 0, 0)).add(days + 1, 'd');
+
+  var difference = end.diff(start, 'days');
+
+  for(var i = 0; i < difference; i++) {
+    dates.push(end.subtract(1, 'd').format(dateFormat));
+  }
+
+  if (ascending) {
+    return dates.reverse();
+  }
+
+  return dates;
+}
+
+module.exports = {
+  rangeArray: range,
+  lastNDays: lastNDays,
+  nextNDays: nextNDays
 }
